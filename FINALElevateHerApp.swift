947ae0035dece105bd0f48464f4b288e4e40fe7d
Nikki <nikkiranabhat@gmail.com>
2025-07-30 17:34:28 -0400

    testing

---
+++ FINALElevateHerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FINALElevateHerApp.swift
+//  FINALElevateHer
+//
+//  Created by Nikki  on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct FINALElevateHerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ New Group/FINALElevateHer/Profile-Saanvi.swift
@@ -0,0 +1,38 @@
+//
+//  Profile-Saanvi.swift
+//  FINALElevateHer
+//
+//  Created by Sunil Bhosekar on 7/30/25.
+//
+
+import SwiftUI
+
+struct Profile_Saanvi: View {
+    var body: some View {
+        ZStack{
+            Color(hex:0xF49CBB)
+                .ignoresSafeArea()
+            VStack{
+                Text("Welcome to your Profile!")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Spacer()
+                Image("Profile Icon")
+                    .resizable()
+                Spacer()
+                Spacer()
+                Text("Name")
+                Text("Job")
+                Text("Years of Experience: ")
+                Text("Graduated from: ")
+                
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    Profile_Saanvi()
+}
+
