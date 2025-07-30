cbab40f3bef3417ca93f7bd35a9c6ca913c230f4
Tanisha <tanishamodi.mt@gmail.com>
2025-07-30 14:36:46 -0400

    Testing for GitHub

---
+++ FINALElevateHer/tanishaPosts.swift
@@ -0,0 +1,18 @@
+//
+//  tanishaPosts.swift
+//  FINALElevateHer
+//
+//  Created by Scholar on 7/30/25.
+//
+
+import SwiftUI
+
+struct tanishaPosts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    tanishaPosts()
+}
